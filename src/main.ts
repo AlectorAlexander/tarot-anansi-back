9e20ea15fd8a7104f81dfe53120f3f23a6b65790
Aléctor Alexander <alector.alexandre@gmail.com>
2023-07-04 19:19:27 -0300

    build: conexão feita com o banco de dados MongoDB

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MongoClient } from 'mongodb';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const uri = process.env.MONGO_DB_URL;
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    console.log('Conectado ao MongoDB!');
+
+    const db = client.db(); // Get a reference to the database
+    const result = await db.command({ ping: 1 }); // Send a ping command to the database
+    console.log('Teste de conexão bem-sucedido:', result);
+
+    await app.listen(process.env.PORT);
+    console.log(`Aplicação iniciada na porta ${process.env.PORT}`);
+  } catch (error) {
+    console.error('Erro ao conectar ao MongoDB', error);
+  }
 }
+
 bootstrap();
